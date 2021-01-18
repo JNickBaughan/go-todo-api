368d5a0d6181850cb877495ee6821c591e95d260
JNickBaughan <JNickBaughan@gmail.com>
2021-01-18 17:03:07 -0500

    add delete route

--- main.go
+++ main.go
@@ -9,6 +9,20 @@ import (
 	"io/ioutil"
 	"go-todo-api/models"
 )
+
+func indexOf(id string, todos []models.Todo) int {
+	for i, t := range todos {
+		if  id == t.ID {
+			return i
+		}
+	}
+	return -1    //not found.
+ }
+
+ func removeTodo(i int, todos []models.Todo) []models.Todo {
+	copy(todos[i:], todos[i+1:]) // Shift a[i+1:] left one index.
+	return todos[:len(todos)-1] 
+ }
  
 var todos []models.Todo
 
@@ -34,11 +48,29 @@ func listTodos(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, string(todoList))
 }
 
+func deleteTodo(w http.ResponseWriter, r *http.Request) {
+	todoID := mux.Vars(r)["id"]
+	
+	todos = removeTodo(indexOf(todoID, todos), todos)
+
+	updatedList, e := json.Marshal(todos)
+	
+	if e != nil {
+		fmt.Fprintf(w, "Uh Oh... something went wrong when trying to delete that todo")
+	}
+
+	fmt.Fprintf(w, string(updatedList))
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", listTodos)
 
 	router.HandleFunc("/add", addTodo)
+
+	router.HandleFunc("/delete/{id}", deleteTodo)
+
+
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
